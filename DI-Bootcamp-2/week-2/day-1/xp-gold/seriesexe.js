784450b2aa100b4f93d42e34b376dc4d9a23dbac
jacobgreg27 <122863612+jacobgreg27@users.noreply.github.com>
2023-01-23 23:27:42 +0400

    Add files via upload

---
+++ DI-Bootcamp-2/week-2/day-1/xp-gold/seriesexe.js
@@ -0,0 +1,12 @@
+const myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+console.log(myWatchedSeries);
+
+const myWatchedSeriesLength  = myWatchedSeries.length
+
+console.log(myWatchedSeriesLengt   = myWatchedSeries.length);
+
+
+const myWatchedSeriesSentence = `${myWatchedSeries[0]}, ${myWatchedSeries[1]} and ${myWatchedSeries[2]}`
+
+console.log(myWatchedSeriesSentence = `${myWatchedSeries[0]}, ${myWatchedSeries[1]} and ${myWatchedSeries[2]}`)
