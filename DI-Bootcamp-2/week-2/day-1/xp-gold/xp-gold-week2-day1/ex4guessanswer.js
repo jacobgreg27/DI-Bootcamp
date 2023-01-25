7e9f1c7a441d89be5e7147c64ab364f7c2192afd
jacobgreg27 <122863612+jacobgreg27@users.noreply.github.com>
2023-01-25 09:08:43 +0400

    Add files via upload

---
+++ DI-Bootcamp-2/week-2/day-1/xp-gold/xp-gold-week2-day1/ex4guessanswer.js
@@ -0,0 +1,19 @@
+let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b) //1 It is operating an addition of the numbers 34 and 21
+    // Prediction: 55
+    // Actual: 55
+
+    a = 2;
+
+    console.log(a+b) //2 It is operating an addition of the numbers 2 and 21 
+    // Prediction: 23
+    // Actual: 23
+
+    //3 c is undefined
+
+   
+    console.log(3 + 4 + '5'); //4 Concated since it could not add all the numbers for 5 is a string
+    
---
+++ DI-Bootcamp-2/week-2/day-1/xp-gold/xp-gold-week2-day1/tempex3.js
@@ -0,0 +1,5 @@
+const degcelcius = 10
+
+const degfarenheit = 10 / 5 * 9 + 32
+
+console.log(`${degcelcius} Degree Celcius is ${degfarenheit} Degrees Farenheit`)
