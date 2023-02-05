b1c490df16442e57de03381e4303b9cc60b84f5d
jacobgreg27 <122863612+jacobgreg27@users.noreply.github.com>
2023-02-05 20:42:55 +0400

    Add files via upload

---
+++ DI-Bootcamp-2/week-2/day-1/xp-exe-week2-day-day4/information.js
@@ -0,0 +1,19 @@
+infoAboutMe()
+
+
+function infoAboutMe(){
+    console.log("my name is jacob")
+}
+
+
+
+infoAboutPerson("Laura", 19, "blue")
+infoAboutPerson("Jean", 20, "grey")
+
+function infoAboutPerson(personName, personAge, personFavoriteColor){
+    console.log("Your name is", personName)
+    console.log("You are", personAge, "years old")
+    console.log("Your favorite color is", personFavoriteColor)
+
+}
+
